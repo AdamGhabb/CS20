14dbb2edb87e3f75c3de7161a68dc750d29641ab
1100071639 <1100071639@L815WD220033.edu.cbe.ab.ca>
2022-09-12 11:58:03 -0600

    Lesson 2 of java

--- Chapter 2/src/Chapter2.java
+++ Chapter 2/src/Chapter2.java
@@ -3,7 +3,26 @@ public class Chapter2 extends TicTacToe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("------------------------- \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("------------------------- \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
+		System.out.print("\t| \t");
+		System.out.print("| \n");
 	}
 
 }
---
+++ Chapter 2/src/MyInitials.java
@@ -0,0 +1,25 @@
+
+public class MyInitials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("\t   __ \n");
+		System.out.print("\t  |  | \n");
+		System.out.print("\t |    | \n");
+		System.out.print("\t|      ");
+		System.out.print("| \n");
+		System.out.print("       |        ");
+		System.out.print("| \n");
+		System.out.print("      |          ");
+		System.out.print("| \n");
+		System.out.print("     -------------- \n");
+		System.out.print("    |              ");
+		System.out.print("| \n");
+		System.out.print("   |                ");
+		System.out.print("| \n");
+		System.out.print("  |                  ");
+		System.out.print("| \n");
+
+	}
+
+}
