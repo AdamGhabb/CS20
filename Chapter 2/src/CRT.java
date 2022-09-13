c437b17a677477ec32e2e025cdc8e73de1440a13
1100071639 <1100071639@L815WD220033.edu.cbe.ab.ca>
2022-09-13 10:25:04 -0600

    10:24 am

---
+++ Chapter 2/src/CRT.java
@@ -0,0 +1,36 @@
+
+public class CRT
+{
+
+	
+	public static void main(String[] args) 
+	{
+		/*
+		 
+		 
+		3.) Average.java
+		 	Calculates test averages
+		 	Adam Ghabboura
+		 	September, 13, 2022
+		 
+		 
+		
+		4.) The AverageScore class displays the average set of scores
+		 
+		
+		5.) A package with the name GradeCalculator
+		
+		
+		6.) Class AverageScore is available to any code
+		
+		 
+		7.)	A source code is a code typed by the program.
+		 	A byte code is when the source code is compiled.
+		 	
+		 	
+		12.) Println moves the cursor to a new line. Print doesnt move the cursor. 
+		
+		 */
+	}
+
+}
---
+++ Chapter 2/src/SkillBuildingCH3.java
@@ -0,0 +1,18 @@
+
+public class SkillBuildingCH3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int length = 13;
+		int width =4;
+		int area;
+		int perimeter;
+		
+		area = length * width;
+		perimeter = length + length + width + width;
+		System.out.println("Area of rectangle: " + area);
+		System.out.println("Perimeter of rectangle: " + perimeter);
+	}
+
+}
