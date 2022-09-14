792ea98f366fa191997ab6b4e27a867ad523a983
1100071639 <1100071639@L815WD220033.edu.cbe.ab.ca>
2022-09-14 11:37:13 -0600

    11:37am

--- Chapter 2/src/Testing.java
+++ Chapter 2/src/Testing.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
 
 public class Testing {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner userinput = new Scanner(System.in);
+		
+		int age;
+		double salary;
+		String name;
+		char FirstInitial;
 
+		System.out.println("Please enter your full name: ");
+		
+		name = userinput.nextLine();
+		
+		System.out.println("The name you have entered is: " + name);
+		
+		System.out.println("Please enter your age: ");
+		
+		
+		age = userinput.nextInt();
+		
+		
+		System.out.println("The name you have entered is: " + age);
+		
+		System.out.println("Please enter your Salary: ");
+		
+		
+		salary = userinput.nextLong();
+		
+		
+		System.out.println("The Salary you have entered is: " + salary);
+		
+		System.out.print("To confirm everything your name is " + name ". And your age is  + age ". "And your salary is " + salary );
 	}
 
 }
---
+++ Chapter3/src/SkillBuilding/RectanglePeri.java
@@ -0,0 +1,19 @@
+package SkillBuilding;
+
+public class RectanglePeri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int length = 13;
+		int width =4;
+		int area;
+		int perimeter;
+		
+		area = length * width;
+		perimeter = length + length + width + width;
+		System.out.println("Area of rectangle: " + area);
+		System.out.println("Perimeter of rectangle: " + perimeter);
+	}
+	
+}
